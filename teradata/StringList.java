dae046eea16303383c638e55892bf5835e366879
apoorvedave <a1dave@ucsd.edu>
2016-03-21 20:31:36 -0700

    Formatting changes

--- teradata/StringList.java
+++ teradata/StringList.java
@@ -8,22 +8,21 @@ public class StringList {
 	private StringNode head;
 	private StringNode tail;
 	private int count = 0;
-	
+
 	/*
-	 *  Method to insert at end of list
+	 * Method to insert at end of list
 	 */
 	public void insert(String s) {
 		StringNode node = new StringNode(s);
 		if (head == null) {
 			head = node;
-		}
-		else {
+		} else {
 			tail.setRight(node);
 		}
 		tail = node;
 		count++;
 	}
-	
+
 	/*
 	 * Method to return an array of strings from StringList object
 	 */
@@ -31,17 +30,17 @@ public class StringList {
 		if (count == 0) {
 			return null;
 		}
-		String [] strings = new String[count];
+		String[] strings = new String[count];
 		StringNode node = head;
-		int i=0;
-		while(node!=null) {
+		int i = 0;
+		while (node != null) {
 			strings[i] = node.getValue();
 			i++;
-			node=node.getRight();
+			node = node.getRight();
 		}
 		return strings;
 	}
-	
+
 	/*
 	 * Some Test Code
 	 */
@@ -54,7 +53,7 @@ public class StringList {
 		sl.insert("bac");
 		System.out.println(sl.head.getValue());
 		System.out.println(sl.tail.getValue());
-		
+
 		System.out.println(sl.getStrings()[1]);
 	}
 }
