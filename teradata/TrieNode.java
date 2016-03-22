66ca0614e4a749affe9a34355e6de4d49d3aa442
apoorvedave <a1dave@ucsd.edu>
2016-03-21 20:35:22 -0700

    Formatting changes

--- teradata/TrieNode.java
+++ teradata/TrieNode.java
@@ -7,15 +7,20 @@ public class TrieNode {
 
 	// Variable to hold word meaning.
 	// null means the word is not terminated on this trie node.
-	// I have overloaded the meaning variable to store the meaning (if non null) as well as 
-	// a flag whose non null value represents the end of word from the beginning of trie. 
+	// I have overloaded the meaning variable to store the meaning (if non null)
+	// as well as
+	// a flag whose non null value represents the end of word from the beginning
+	// of trie.
 	private String meaning;
-	
-	// Store mappings of children nodes (represents set of possible next characters
-	// Assumption: ascii and non-ascii characters both belong to unicode character set. 
-	// (NOTE: I am using Character as key here with an assumption(fact) that all the above mentioned characters can also be represented by Character
+
+	// Store mappings of children nodes (represents set of possible next
+	// characters
+	// Assumption: ascii and non-ascii characters both belong to unicode
+	// character set.
+	// (NOTE: I am using Character as key here with an assumption(fact) that all
+	// the above mentioned characters can also be represented by Character
 	private HashMap<Character, TrieNode> children;
-	
+
 	public String getMeaning() {
 		return meaning;
 	}
