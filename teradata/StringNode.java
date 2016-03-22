16bbbc823570a7c7fae338f63b9ef75dcd06d834
apoorvedave <a1dave@ucsd.edu>
2016-03-21 20:32:11 -0700

    Formatting Changes

--- teradata/StringNode.java
+++ teradata/StringNode.java
@@ -5,24 +5,31 @@ public class StringNode {
 	private String value;
 	private StringNode left;
 	private StringNode right;
+
 	public StringNode(String value) {
 		this.value = value;
 	}
+
 	public String getValue() {
 		return value;
 	}
+
 	public void setValue(String value) {
 		this.value = value;
 	}
+
 	public StringNode getLeft() {
 		return left;
 	}
+
 	public void setLeft(StringNode left) {
 		this.left = left;
 	}
+
 	public StringNode getRight() {
 		return right;
 	}
+
 	public void setRight(StringNode right) {
 		this.right = right;
 	}
